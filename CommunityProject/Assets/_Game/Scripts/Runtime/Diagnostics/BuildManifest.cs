aafa247f4e70cbe9acbb7f8b56b4013be79be77f
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2022-09-16 14:08:43 +0200

    feat: add BuildManifestReader

---
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Diagnostics/BuildManifest.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+namespace BoundfoxStudios.CommunityProject.Diagnostics
+{
+	/// <summary>
+	/// This class abstracts the manifest.json file.
+	/// </summary>
+	[Serializable]
+	public class BuildManifest
+	{
+		[SerializeField]
+		// ReSharper disable once InconsistentNaming
+		private string sha;
+
+		[SerializeField]
+		// ReSharper disable once InconsistentNaming
+		private string shortSha;
+
+		[SerializeField]
+		// ReSharper disable once InconsistentNaming
+		private int runId;
+
+		[SerializeField]
+		// ReSharper disable once InconsistentNaming
+		private int runNumber;
+
+		public string Sha => sha;
+		public string ShortSha => shortSha;
+		public int RunId => runId;
+		public int RunNumber => runNumber;
+	}
+}
