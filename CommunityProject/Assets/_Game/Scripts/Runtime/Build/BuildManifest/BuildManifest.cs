30195ea7b04cc8436dc9e10b9a2c79fbb6b44fe5
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2022-09-19 14:31:42 +0200

    refactor: move BuildManifest to Build/BuildManifest folder

--- CommunityProject/Assets/_Game/Scripts/Runtime/Build/BuildManifest/BuildManifest.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Build/BuildManifest/BuildManifest.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-namespace BoundfoxStudios.CommunityProject.Diagnostics
+namespace BoundfoxStudios.CommunityProject.Build.BuildManifest
 {
 	/// <summary>
 	/// This class abstracts the manifest.json file.
