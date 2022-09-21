25ab8b2f8b57872fb775a77489478cd6037628c7
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2022-09-21 13:50:17 +0200

    feat: add basic event infrastructure
    fixes #22

---
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Events/ScriptableObjects/VoidEventChannelSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace BoundfoxStudios.CommunityProject.Events.ScriptableObjects
+{
+	[CreateAssetMenu(menuName = Constants.MenuNames.Events + "/Void Event Channel")]
+	public class VoidEventChannelSO : EventChannelSO
+	{
+		// Marker class
+	}
+}
