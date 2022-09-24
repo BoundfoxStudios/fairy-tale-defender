9b35b15305f469a2742406aa5c7021ef42996146
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2022-09-24 13:39:16 +0200

    delete this

--- CommunityProject/Assets/_Game/Scripts/Runtime/UI/ViewBuildNumber.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/UI/ViewBuildNumber.cs
@@ -11,24 +11,24 @@ namespace BoundfoxStudios.CommunityProject.UI
 	public class ViewBuildNumber : MonoBehaviour
 	{
 		private TextMeshProUGUI _buildNumberText;
-		private BuildManifestReader bmReader = new BuildManifestReader();
+		private BuildManifestReader _bmReader = new BuildManifestReader();
 
-		private BuildManifest unibm;
+		private BuildManifest _buildManifest;
 		private Action LoadBuildManifestAsyncAction;
 
 		private void InitLoadBuildManifestAsyncAction()
 		{
 			LoadBuildManifestAsyncAction = new Action(async () =>
 			{
-				unibm = await bmReader.LoadAsync();
-				_buildNumberText.text = $"Build: {Application.version} ({unibm.ShortSha})";
+				_buildManifest = await _bmReader.LoadAsync();
+				_buildNumberText.text = $"Build: {Application.version} ({_buildManifest.ShortSha})";
 			});
 		}
 
 		// Start is called before the first frame update
 		void Awake()
 		{
-			_buildNumberText = this.gameObject.GetComponent<TextMeshProUGUI>();
+			_buildNumberText = gameObject.GetComponent<TextMeshProUGUI>();
 			InitLoadBuildManifestAsyncAction();
 			LoadBuildManifestAsyncAction.Invoke();
 		}
