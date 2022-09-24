a9235fd76c03bf1aee659fcb1b4855fb5183029a
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2022-09-24 13:23:43 +0200

    change canvas to existing prefab canves, spaces to tabs and using code conventions

--- CommunityProject/Assets/_Game/Scripts/Runtime/UI/ViewBuildNumber.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/UI/ViewBuildNumber.cs
@@ -7,30 +7,30 @@ using System;
 
 namespace BoundfoxStudios.CommunityProject.UI
 {
-    [AddComponentMenu(Constants.MenuNames.UI + "/" + nameof(ViewBuildNumber))]
-    public class ViewBuildNumber : MonoBehaviour
-    {
-        private TextMeshProUGUI _buildNumberText;
-        private BuildManifestReader bmReader = new BuildManifestReader();
+	[AddComponentMenu(Constants.MenuNames.UI + "/" + nameof(ViewBuildNumber))]
+	public class ViewBuildNumber : MonoBehaviour
+	{
+		private TextMeshProUGUI _buildNumberText;
+		private BuildManifestReader bmReader = new BuildManifestReader();
 
-        public BuildManifest unibm;
-        private Action LoadBuildManifestAsyncAction;
+		private BuildManifest unibm;
+		private Action LoadBuildManifestAsyncAction;
 
-        private void InitLoadBuildManifestAsyncAction()
-        {
-            LoadBuildManifestAsyncAction = new Action(async () =>
-            {
-                unibm = await bmReader.LoadAsync();
-                _buildNumberText.text = $"Build: {Application.version} ({unibm.ShortSha})";
-            });
-        }
+		private void InitLoadBuildManifestAsyncAction()
+		{
+			LoadBuildManifestAsyncAction = new Action(async () =>
+			{
+				unibm = await bmReader.LoadAsync();
+				_buildNumberText.text = $"Build: {Application.version} ({unibm.ShortSha})";
+			});
+		}
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            _buildNumberText = this.gameObject.GetComponent<TextMeshProUGUI>();
-            InitLoadBuildManifestAsyncAction();
-            LoadBuildManifestAsyncAction.Invoke();
-        }
-    }
+		// Start is called before the first frame update
+		void Start()
+		{
+			_buildNumberText = this.gameObject.GetComponent<TextMeshProUGUI>();
+			InitLoadBuildManifestAsyncAction();
+			LoadBuildManifestAsyncAction.Invoke();
+		}
+	}
 }
