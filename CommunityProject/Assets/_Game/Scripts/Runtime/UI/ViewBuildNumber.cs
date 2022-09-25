fed1bfef9d10b96f1b783a5407dfd257b834dd95
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2022-09-25 17:13:38 +0200

    CreateBuildNumberAsync and Code Convesion

--- CommunityProject/Assets/_Game/Scripts/Runtime/UI/ViewBuildNumber.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/UI/ViewBuildNumber.cs
@@ -10,29 +10,23 @@ namespace BoundfoxStudios.CommunityProject.UI
 {
 	[AddComponentMenu(Constants.MenuNames.UI + "/" + nameof(ViewBuildNumber))]
 	[RequireComponent(typeof(TextMeshProUGUI))]
-
 	public class ViewBuildNumber : MonoBehaviour
 	{
 		private TextMeshProUGUI _buildNumberText;
-		private BuildManifestReader _buildManifestReader = new BuildManifestReader();
 
-		private async UniTask CreateBuildNumber()
-		{
-			var buildManifest = await _buildManifestReader.LoadAsync();
-			_buildNumberText.text = $"Build: {Application.version} ({buildManifest.ShortSha})";
-		}
-
-		// Start is called before the first frame update
 		private async UniTaskVoid Awake()
 		{
 			_buildNumberText = gameObject.GetComponent<TextMeshProUGUI>();
-
-			var buildManifest = await _buildManifestReader.LoadAsync();
-
-			_buildNumberText.text = $"Build: {Application.version} ({buildManifest.ShortSha})";
+			_buildNumberText.text = await CreateBuildNumberAsync();
 		}
 
-		// Kopiert die Buildnummer in die Windows Zwischenablage
+		private async UniTask<string> CreateBuildNumberAsync()
+		{
+			BuildManifestReader _buildManifestReader = new BuildManifestReader();
+			var buildManifest = await _buildManifestReader.LoadAsync();
+			return $"Build: {Application.version} ({buildManifest.ShortSha})";
+		}
+
 		public void CopyBuildNumberToClipboard()
 		{
 			GUIUtility.systemCopyBuffer = _buildNumberText.text;
