8712f8698f1cdcf1f509d3e8a8ca0cac75d52c7b
Christian Schamara <schamara.christian@gmail.com>
2022-09-29 08:59:23 +0200

    add: InputReaderSO
    Basic implementaion of basic "InputReaderSO" according to https://github.com/BoundfoxStudios/community-project/issues/69
    
    Signed-off-by: Christian Schamara <schamara.christian@gmail.com>

---
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Input/InputReaderSO.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace BoundfoxStudios.CommunityProject.Input
+{
+	[CreateAssetMenu(menuName = Constants.MenuNames.Input + "/Input Reader")]
+	public class InputReaderSO : ScriptableObject, GameInput.IGameplayActions, GameInput.IUIActions
+	{
+		private GameInput _gameInput;
+
+		private void OnEnable()
+		{
+			if (_gameInput == null)
+			{
+				_gameInput = new GameInput();
+
+				_gameInput.Gameplay.SetCallbacks(this);
+				_gameInput.UI.SetCallbacks(this);
+			}
+		}
+
+		private void OnDisable()
+		{
+			DisableAllInput();
+		}
+
+		public void DisableAllInput()
+		{
+			_gameInput.Gameplay.Disable();
+			_gameInput.UI.Disable();
+		}
+
+		public void EnableGameplayInput()
+		{
+			_gameInput.UI.Disable();
+			_gameInput.Gameplay.Enable();
+		}
+
+		public void EnableUIInput()
+		{
+			_gameInput.Gameplay.Disable();
+			_gameInput.UI.Enable();
+		}
+	}
+}
