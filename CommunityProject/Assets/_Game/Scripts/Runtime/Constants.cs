2b784357ca040d8c7d07b84df17633d7147241f9
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2022-10-18 17:05:52 +0200

    Update Constants.cs

--- CommunityProject/Assets/_Game/Scripts/Runtime/Constants.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Constants.cs
@@ -5,7 +5,7 @@ namespace BoundfoxStudios.CommunityProject
 		public static class MenuNames
 		{
 			public const string MenuName = "Community Project";
-			public const string Events = MenuName + "/Events";
+			public const string Events = MenuNamee + "/Events";
 			public const string SceneManagement = MenuName + "/Scene Management";
 			public const string UI = MenuName + "/UI";
 			public const string Input = MenuName + "/Input";
