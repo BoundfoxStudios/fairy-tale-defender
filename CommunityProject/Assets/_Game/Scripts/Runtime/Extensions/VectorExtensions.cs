b2778782fa1bc72dfd6e24b916232173c6c69b0f
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-01-23 14:28:00 +0100

    refactor(VectorExtensions): use sqrMagnitude

--- CommunityProject/Assets/_Game/Scripts/Runtime/Extensions/VectorExtensions.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Extensions/VectorExtensions.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace BoundfoxStudios.CommunityProject.Extensions
@@ -5,7 +6,11 @@ namespace BoundfoxStudios.CommunityProject.Extensions
 	public static class VectorExtensions
 	{
 		public static Vector2 ToXZ(this Vector3 vector) => new(vector.x, vector.z);
-		public static float DistanceSquaredTo(this Vector3 a, Vector3 b) => Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y - b.y, 2);
-		public static float DistanceSquaredTo(this Vector2 a, Vector2 b) => Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y - b.y, 2);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float DistanceSquaredTo(this Vector3 a, Vector3 b) => (b - a).sqrMagnitude;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float DistanceSquaredTo(this Vector2 a, Vector2 b) => (b - a).sqrMagnitude;
 	}
 }
