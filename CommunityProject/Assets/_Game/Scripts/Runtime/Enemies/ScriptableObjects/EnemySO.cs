473cf915beb96f3eab5836330b421f3e8cb4e89a
M4lph <malph@live.de>
2023-01-23 22:23:40 +0100

    real fix for formatting, add default values
    formatting is a very hard and complex task :)

--- CommunityProject/Assets/_Game/Scripts/Runtime/Enemies/ScriptableObjects/EnemySO.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Enemies/ScriptableObjects/EnemySO.cs
@@ -12,31 +12,31 @@ namespace BoundfoxStudios.CommunityProject.Enemies.ScriptableObjects
 		[field: SerializeField]
 		public LocalizedString Name { get; private set; }
 
-		[field: SerializeField,
-				Min(1)]
-		public int MaxHealth { get; private set; }
+		[field: SerializeField]
+		[field: Min(1)]
+		public int MaxHealth { get; private set; } = 1;
 
-		[field: SerializeField,
-				Min(0)]
-		public int Armor { get; private set; }
+		[field: SerializeField]
+		[field: Min(0)]
+		public int Armor { get; private set; } = 1;
 
-		[field: SerializeField,
-				Min(0)]
-		public float MovementSpeed { get; private set; }
+		[field: SerializeField]
+		[field: Min(0)]
+		public float MovementSpeed { get; private set; } = 1f;
 
-		[field: SerializeField,
-				Min(0),
-				Tooltip("Size of this enemy in meter.")]
-		public float Size { get; private set; }
+		[field: SerializeField]
+		[field: Min(0)]
+		[field: Tooltip("Size of this enemy in meter.")]
+		public float Size { get; private set; } = 1f;
 
-		[field: SerializeField,
-				Min(0),
-				Tooltip("The amount of experience awarded to player on kill.")]
-		public int ExperienceOnKill { get; private set; }
+		[field: SerializeField]
+		[field: Min(0)]
+		[field: Tooltip("The amount of experience awarded to player on kill.")]
+		public int ExperienceOnKill { get; private set; } = 1;
 
-		[field: SerializeField,
-				Min(0),
-				Tooltip("The amount of currency awarded to player on kill.")]
-		public int CurrencyOnKill { get; private set; }
+		[field: SerializeField]
+		[field: Min(0)]
+		[field: Tooltip("The amount of currency awarded to player on kill.")]
+		public int CurrencyOnKill { get; private set; } = 1;
 	}
 }
