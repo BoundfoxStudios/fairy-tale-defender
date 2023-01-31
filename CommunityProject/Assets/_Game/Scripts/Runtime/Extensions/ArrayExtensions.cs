b00831cf68cce000f6fbfd991c73aa57b9a2c20c
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-01-31 13:44:37 +0100

    refactor(ArrayExtension): update syntax

--- CommunityProject/Assets/_Game/Scripts/Runtime/Extensions/ArrayExtensions.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Extensions/ArrayExtensions.cs
@@ -6,7 +6,7 @@ namespace BoundfoxStudios.CommunityProject.Extensions
 	{
 		public static T PickRandom<T>(this T[]? items)
 		{
-			Debug.Assert(items != null, "Trying to pick a random from a non-existing array!");
+			Debug.Assert(items is not null, "Trying to pick a random from a non-existing array!");
 			Debug.Assert(items!.Length > 0, "Trying to pick a random from an empty array!");
 
 			return items[Random.Range(0, items.Length)];
