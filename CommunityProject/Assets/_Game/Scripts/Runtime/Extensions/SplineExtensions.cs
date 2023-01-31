d1f62ace4d608cc97ebcefd365c507825d75fb66
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-01-31 13:45:07 +0100

    refactor(Spline|Vector): MethodImpl

--- CommunityProject/Assets/_Game/Scripts/Runtime/Extensions/SplineExtensions.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Extensions/SplineExtensions.cs
@@ -1,9 +1,11 @@
+using System.Runtime.CompilerServices;
 using UnityEngine.Splines;
 
 namespace BoundfoxStudios.CommunityProject.Extensions
 {
 	public static class SplineExtensions
 	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static bool IsValidKnotIndex(this ISpline spline, int knotIndex) => knotIndex >= 0 && knotIndex < spline.Count;
 	}
 }
