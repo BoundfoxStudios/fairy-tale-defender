c413cdc4b032397334701fd28d4cab54f6d62653
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-01-31 13:08:41 +0100

    feat: Guard.AgainstNull to make in-editor null checks easier and less verbose to write

---
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Infrastructure/Guard.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using Debug = UnityEngine.Debug;
+
+namespace BoundfoxStudios.CommunityProject.Infrastructure
+{
+	public static class Guard
+	{
+		[Conditional("UNITY_ASSERTIONS")]
+		public static void AgainstNull<T>(Expression<Func<T>> expression)
+		{
+			if (expression.Body is not MemberExpression member)
+			{
+				throw new ArgumentException($"Expression {expression} refers to a method, not a property or field");
+			}
+
+			var method = expression.Compile();
+			var @object = method();
+
+			Debug.Assert(@object is not null, $"{member.Member.Name} is null");
+		}
+	}
+}
