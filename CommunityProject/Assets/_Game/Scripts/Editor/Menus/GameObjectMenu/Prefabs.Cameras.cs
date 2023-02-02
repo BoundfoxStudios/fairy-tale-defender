ca621b3eef87c901ffa41cfa3b57ad0433af2129
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-02-02 08:46:32 +0100

    refactor(PrefabsMenu): place code into their correct files

--- CommunityProject/Assets/_Game/Scripts/Editor/Menus/GameObjectMenu/Prefabs.Cameras.cs
+++ CommunityProject/Assets/_Game/Scripts/Editor/Menus/GameObjectMenu/Prefabs.Cameras.cs
@@ -5,11 +5,11 @@ namespace BoundfoxStudios.CommunityProject.Editor.Menus.GameObjectMenu
 {
 	public static partial class Prefabs
 	{
-		[MenuItem(Constants.MenuNames.GameObjectMenus.Lights + "/Sun", priority = LightsMenuPriority)]
+		[MenuItem(Constants.MenuNames.GameObjectMenus.Cameras + "/Menu", priority = CamerasMenuPriority)]
 		// ReSharper disable once Unity.IncorrectMethodSignature
-		private static async UniTaskVoid CreateSunAsync()
+		private static async UniTaskVoid CreateMenuCameraAsync()
 		{
-			await SafeInstantiateAsync(prefabManager => prefabManager.Lights.Sun);
+			await SafeInstantiateAsync(prefabManager => prefabManager.Cameras.Menu);
 		}
 	}
 }
--- CommunityProject/Assets/_Game/Scripts/Editor/Menus/GameObjectMenu/Prefabs.Lights.cs
+++ CommunityProject/Assets/_Game/Scripts/Editor/Menus/GameObjectMenu/Prefabs.Lights.cs
@@ -5,11 +5,11 @@ namespace BoundfoxStudios.CommunityProject.Editor.Menus.GameObjectMenu
 {
 	public static partial class Prefabs
 	{
-		[MenuItem(Constants.MenuNames.GameObjectMenus.Cameras + "/Menu", priority = CamerasMenuPriority)]
+		[MenuItem(Constants.MenuNames.GameObjectMenus.Lights + "/Sun", priority = LightsMenuPriority)]
 		// ReSharper disable once Unity.IncorrectMethodSignature
-		private static async UniTaskVoid CreateMenuCameraAsync()
+		private static async UniTaskVoid CreateSunAsync()
 		{
-			await SafeInstantiateAsync(prefabManager => prefabManager.Cameras.Menu);
+			await SafeInstantiateAsync(prefabManager => prefabManager.Lights.Sun);
 		}
 	}
 }
