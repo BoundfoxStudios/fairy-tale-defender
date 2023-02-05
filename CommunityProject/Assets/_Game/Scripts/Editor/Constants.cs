97d33071c9f31781766a76781b7d385ebcdf356c
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-02-05 12:25:11 +0100

    feat: add Bars UI

--- CommunityProject/Assets/_Game/Scripts/Editor/Constants.cs
+++ CommunityProject/Assets/_Game/Scripts/Editor/Constants.cs
@@ -14,6 +14,7 @@ namespace BoundfoxStudios.CommunityProject.Editor
 			{
 				public const string UI = GameObjectRoot + "/UI";
 				public const string Texts = UI + "/Texts";
+				public const string Bars = UI + "/Bars";
 
 				public const string Cameras = GameObjectRoot + "/Cameras";
 				public const string Lights = GameObjectRoot + "/Lights";
