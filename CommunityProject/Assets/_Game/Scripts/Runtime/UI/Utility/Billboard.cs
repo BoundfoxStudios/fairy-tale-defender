9c2aae56773dc24e9818bb4c5543bb2880e7d9cc
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-02-05 12:24:18 +0100

    feat: add UI Billboard script

---
+++ CommunityProject/Assets/_Game/Scripts/Runtime/UI/Utility/Billboard.cs
@@ -0,0 +1,21 @@
+using BoundfoxStudios.CommunityProject.Infrastructure.RuntimeAnchors.ScriptableObjects;
+using UnityEngine;
+
+namespace BoundfoxStudios.CommunityProject.UI.Utility
+{
+	[AddComponentMenu(Constants.MenuNames.UI + "/" + nameof(Billboard))]
+	public class Billboard : MonoBehaviour
+	{
+		[field: SerializeField]
+		private CameraRuntimeAnchorSO CameraRuntimeAnchor { get; set; } = default!;
+
+		private void LateUpdate()
+		{
+			if (CameraRuntimeAnchor.TryGetItem(out var mainCamera))
+			{
+				transform.LookAt(transform.position + mainCamera.transform.forward);
+			}
+		}
+	}
+}
+
