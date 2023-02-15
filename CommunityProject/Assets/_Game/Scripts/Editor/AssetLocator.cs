2cebb2605b57286bc591d0a6812cade28c6ab2a2
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-02-15 10:55:59 +0100

    feat: basic infrastructure for a first level

---
+++ CommunityProject/Assets/_Game/Scripts/Editor/AssetLocator.cs
@@ -0,0 +1,45 @@
+using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace BoundfoxStudios.CommunityProject.Editor
+{
+	public class AssetLocator<T>
+		where T: class
+	{
+		private readonly string _addressablesKey;
+		private T? _locatedAsset;
+
+		public AssetLocator(string addressablesKey)
+		{
+			_addressablesKey = addressablesKey;
+		}
+
+		private async UniTask<T?> LocateAssetAsync()
+		{
+			var asset = await Addressables.LoadAssetAsync<T>(_addressablesKey);
+
+			if (asset is null)
+			{
+				var assetTypeName = typeof(T).Name;
+				Debug.LogWarning($"Did not find {assetTypeName} under addressables key {_addressablesKey}");
+				return null;
+			}
+
+			return asset;
+		}
+
+		public async UniTask SafeInvokeAsync(Action<T> callback)
+		{
+			_locatedAsset ??= await LocateAssetAsync();
+
+			if (_locatedAsset is null)
+			{
+				return;
+			}
+
+			callback(_locatedAsset);
+		}
+	}
+}
