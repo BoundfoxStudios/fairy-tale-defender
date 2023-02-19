2a95e7fe5422815f1f83c0f64ad09963a17d21d2
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-02-19 14:43:44 +0100

    test(EffectiveBallisticWeaponCalculatorSO): add tests

---
+++ CommunityProject/Assets/_Game/Scripts/Tests/Editor/Entities/Weapons/BallisticWeapons/ScriptableObjects/EffectiveBallisticWeaponCalculatorSOTests.cs
@@ -0,0 +1,58 @@
+using BoundfoxStudios.CommunityProject.Editor.Extensions;
+using BoundfoxStudios.CommunityProject.Entities.Weapons.BallisticWeapons.ScriptableObjects;
+using BoundfoxStudios.CommunityProject.Entities.Weapons.ScriptableObjects;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace BoundfoxStudios.CommunityProject.Tests.Editor.Entities.Weapons.BallisticWeapons.ScriptableObjects
+{
+	public class EffectiveBallisticWeaponCalculatorSOTests
+	{
+		[TestCase(0, 4)] // No y-Position change -> no change in expected range
+		[TestCase(-10, 4)] // Everything below y < 0 -> no change in expected range
+		[TestCase(1, 6)]
+		[TestCase(2, 8)]
+		[TestCase(10, 24)]
+		public void DoesChangeAddRangeDependingOnTheTowersPosition(float towerYPosition, float expectedMaximumRange,
+			float definedMaximumRange = 4, float heightToRangeFactor = 2)
+		{
+			var sut = CreateSystemUnderTest(heightToRangeFactor);
+			var ballisticWeapon = CreateBallisticWeapon(definedMaximumRange);
+
+			var result = sut.Calculate(ballisticWeapon, new(0, towerYPosition, 0));
+
+			result.MaximumRange.Should().BeApproximately(expectedMaximumRange, 0.001f);
+		}
+
+		private BallisticWeaponSO CreateBallisticWeapon(float maximumRange)
+		{
+			var ballisticWeapon = ScriptableObject.CreateInstance<BallisticWeaponSO>();
+
+			var serializedObject = new SerializedObject(ballisticWeapon);
+			SetPrivateProperty(serializedObject, nameof(BallisticWeaponSO.MinimumRange), 1f);
+
+			// We need to set the base range here, because BallisticWeaponSO redefines "Range" to be a Limit2.
+			SetPrivateProperty(serializedObject, nameof(WeaponSO.Range), maximumRange);
+			return ballisticWeapon;
+		}
+
+		private EffectiveBallisticWeaponCalculatorSO CreateSystemUnderTest(float heightToRangeFactor = 2)
+		{
+			var sut = ScriptableObject.CreateInstance<EffectiveBallisticWeaponCalculatorSO>();
+
+			var serializedObject = new SerializedObject(sut);
+			SetPrivateProperty(serializedObject, nameof(EffectiveBallisticWeaponCalculatorSO.HeightToRangeFactor),
+				heightToRangeFactor);
+			return sut;
+		}
+
+		private void SetPrivateProperty<T>(SerializedObject serializedObject, string propertyName, T value)
+		{
+			var property = serializedObject.FindRealProperty(propertyName);
+			property.SetValue(value);
+			serializedObject.ApplyModifiedPropertiesWithoutUndo();
+		}
+	}
+}
