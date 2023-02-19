5b322198fdf51aa478c605be3f648acdf2e59b9e
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-02-19 14:03:19 +0100

    feat: use tower position instead of weapon position for calculating effective weapon range

--- CommunityProject/Assets/_Game/Scripts/Runtime/Entities/Weapons/BallisticWeapons/ScriptableObjects/EffectiveBallisticWeaponCalculatorSO.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Entities/Weapons/BallisticWeapons/ScriptableObjects/EffectiveBallisticWeaponCalculatorSO.cs
@@ -11,17 +11,17 @@ namespace BoundfoxStudios.CommunityProject.Entities.Weapons.BallisticWeapons.Scr
 		[field: Tooltip("Specifies how much further a target can be reached depending on the height of the weapon.")]
 		public float HeightToRangeFactor { get; private set; } = 1.01f;
 
-		public override EffectiveBallisticWeaponDefinition Calculate(BallisticWeaponSO weaponDefinition, Vector3 weaponPosition)
+		public override EffectiveBallisticWeaponDefinition Calculate(BallisticWeaponSO weaponDefinition, Vector3 towerPosition)
 		{
-			var range = CalculateMaximumRange(weaponDefinition, weaponPosition);
+			var range = CalculateMaximumRange(weaponDefinition, towerPosition);
 			var result = new EffectiveBallisticWeaponDefinition(range, weaponDefinition.FireRateInSeconds, weaponDefinition.AttackAngle);
 			return result;
 		}
 
-		private Limits2 CalculateMaximumRange(BallisticWeaponSO weaponDefinition, Vector3 weaponPosition)
+		private Limits2 CalculateMaximumRange(BallisticWeaponSO weaponDefinition, Vector3 towerPosition)
 		{
 			var range = new Limits2(weaponDefinition.MinimumRange,
-				weaponDefinition.MaximumRange + weaponPosition.y * HeightToRangeFactor);
+				weaponDefinition.MaximumRange + Mathf.Max(0, towerPosition.y) * HeightToRangeFactor);
 			return range;
 		}
 	}
