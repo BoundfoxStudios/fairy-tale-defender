7d3b4f8b2fb26228f43a0f78ec9937cb9b16338f
SuperPirate-ai <minjag@web.de>
2023-02-25 15:59:43 +0100

    fix: check if sceneBuildReport is null

--- CommunityProject/Assets/_Game/Scripts/Editor/OnBuildDestroyObjsManager.cs
+++ CommunityProject/Assets/_Game/Scripts/Editor/OnBuildDestroyObjsManager.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace BoundfoxStudios.CommunityProject.Editor
 {
 	public class OnBuildDestroyObjsManager : IProcessSceneWithReport
 	{
 		public int callbackOrder { get { return 0; } }
-		public void OnProcessScene(UnityEngine.SceneManagement.Scene scene, BuildReport report)
+		public void OnProcessScene(Scene scene, BuildReport report)
 		{
+			if(report == null) return;
 			if (!report.summary.options.HasFlag(UnityEditor.BuildOptions.Development))
 			{
 				GameObject[] objToDestroyOnBuild = GameObject.FindGameObjectsWithTag("DevelopmentOnly");
