d08ba14e10675cc73b2ae0f30a88ce1f49031a17
SuperPirate-ai <minjag@web.de>
2023-02-25 15:41:55 +0100

    refactor(OnBuildDestroyObjsManager)

--- CommunityProject/Assets/_Game/Scripts/Editor/OnBuildDestroyObjsManager.cs
+++ CommunityProject/Assets/_Game/Scripts/Editor/OnBuildDestroyObjsManager.cs
@@ -7,39 +7,21 @@ using UnityEngine;
 
 namespace BoundfoxStudios.CommunityProject.Editor
 {
-    public class OnBuildDestroyObjsManager : IProcessSceneWithReport, IPreprocessBuildWithReport
+	public class OnBuildDestroyObjsManager : IProcessSceneWithReport
 	{
 		public int callbackOrder { get { return 0; } }
-		bool onBuild = false;
-		bool isDevelopmentBuild = false;
-
-
-		public void OnPreprocessBuild(BuildReport report)
-		{
-			onBuild = true;
-			string[] buildOptions = report.summary.options.ToString().Split(',');
-			foreach (var option in buildOptions)
-			{
-				if (option == "developmentbuild")
-				{
-					isDevelopmentBuild = true;
-					return;
-				}
-			}
-		}
-
 		public void OnProcessScene(UnityEngine.SceneManagement.Scene scene, BuildReport report)
 		{
-			GameObject[] objToDestroyOnBuild = GameObject.FindGameObjectsWithTag("DevelopmentOnly");
-			int objToDestroyOnBuildLength = objToDestroyOnBuild.Length;
-			for (int i = 0; i < objToDestroyOnBuildLength; i++)
+			if (!report.summary.options.HasFlag(UnityEditor.BuildOptions.Development))
 			{
-				if (!isDevelopmentBuild && onBuild)
+				GameObject[] objToDestroyOnBuild = GameObject.FindGameObjectsWithTag("DevelopmentOnly");
+				int objToDestroyOnBuildLength = objToDestroyOnBuild.Length;
+				for (int i = 0; i < objToDestroyOnBuildLength; i++)
 				{
 					GameObject.DestroyImmediate(objToDestroyOnBuild[i], true);
 				}
-
 			}
+
 		}
 	}
 }
