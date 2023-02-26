f45c7e897ff1e509875bd8dba0376c99b2a36a18
SuperPirate-ai <minjag@web.de>
2023-02-26 20:13:21 +0100

    fix: implemented requested Suggestions

---
+++ CommunityProject/Assets/_Game/Scripts/Editor/StripDevelopmentOnlyObjectsFromBuild.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BoundfoxStudios.CommunityProject.Editor
+{
+	public class StripDevelopmentOnlyObjectsFromBuild : IProcessSceneWithReport
+	{
+		public int callbackOrder { get; }
+		public void OnProcessScene(Scene scene, BuildReport report)
+		{
+			if (report is null || report.summary.options.HasFlag(BuildOptions.Development))
+			{
+				return;
+			}
+			var objectsToDelete = GameObject.FindGameObjectsWithTag("DevelopmentOnly");
+			for (var i = objectsToDelete.Length - 1; i >= 0; i--)
+			{
+				Object.DestroyImmediate(objectsToDelete[i]);
+			}
+		}
+	}
+}
