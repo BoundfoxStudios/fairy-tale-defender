09aa3506e6e76f49767a92c2ab716eff57e4aa8b
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-04-02 09:07:58 +0200

    feat: rework build controller

---
+++ FairyTaleDefender/Assets/_Game/Scripts/Runtime/Infrastructure/RuntimeAnchors/ScriptableObjects/TransformRuntimeAnchorSO.cs
@@ -0,0 +1,11 @@
+using BoundfoxStudios.FairyTaleDefender.Common;
+using UnityEngine;
+
+namespace BoundfoxStudios.FairyTaleDefender.Infrastructure.RuntimeAnchors.ScriptableObjects
+{
+	[CreateAssetMenu(menuName = Constants.MenuNames.RuntimeAnchors + "/Transform")]
+	public class TransformRuntimeAnchorSO : RuntimeAnchorBaseSO<Transform>
+	{
+		// Marker class
+	}
+}
