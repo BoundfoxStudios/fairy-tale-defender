0732e3171df9cbe5069edf0076876d38df79f7d3
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-04-07 17:35:14 +0200

    fix(tooltip-system): remove unnecessary indirection

--- CommunityProject/Assets/_Game/Scripts/Runtime/Systems/TooltipSystem/Tooltip.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Systems/TooltipSystem/Tooltip.cs
@@ -12,13 +12,11 @@ namespace BoundfoxStudios.CommunityProject.Systems.TooltipSystem
 		[field: SerializeField]
 		public VoidEventChannelSO HideTooltipEventChannel { get; private set; } = default!;
 
-		protected abstract ITooltip GetTooltip();
-
 		public void OnPointerEnter(PointerEventData eventData)
 		{
 			ShowTooltipEventChannel.Raise(new()
 			{
-				Tooltip = GetTooltip(),
+				Tooltip = this,
 				ScreenPosition = eventData.position
 			});
 		}
