28cc6519b9c322f38b9f4af8ca8deed17a3d1020
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-04-07 17:25:43 +0200

    refactor: casing of ToolTip file

--- CommunityProject/Assets/_Game/Scripts/Runtime/Systems/TooltipSystem/TextTooltip.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Systems/TooltipSystem/TextTooltip.cs
@@ -4,7 +4,7 @@ using UnityEngine.Localization;
 namespace BoundfoxStudios.CommunityProject.Systems.TooltipSystem
 {
 	[AddComponentMenu(Constants.MenuNames.UI + "/" + nameof(TextTooltip))]
-	public class TextTooltip : Tooltip, ITextTooltip
+	public class TextTooltip : Tooltip2, ITextTooltip
 	{
 		[field: SerializeField]
 		public LocalizedString Text { get; private set; } = default!;
