169b351a2144eec7b76e00515414921c9859c1c3
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-04-10 14:12:20 +0200

    feat: steam startup locale selector

--- FairyTaleDefender/Assets/_Game/Scripts/Common/Integrations/Steam/SteamManager.cs
+++ FairyTaleDefender/Assets/_Game/Scripts/Common/Integrations/Steam/SteamManager.cs
@@ -17,6 +17,8 @@ namespace BoundfoxStudios.FairyTaleDefender.Common.Integrations.Steam
 	[AddComponentMenu("")] // Hide in component menu.
 	public class SteamManager : MonoBehaviour
 	{
+		public virtual SteamApps SteamApps { get; } = new();
+
 		public virtual void Initialize()
 		{
 			Debug.Log("Steam integration has been turned off. To enable go to Fairy Tale Defender -> Windows -> Steam integration");
