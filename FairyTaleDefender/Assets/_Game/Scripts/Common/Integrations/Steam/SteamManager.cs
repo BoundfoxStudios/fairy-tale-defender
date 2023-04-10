dbee873d39b45b002d2052f0e7abe08f26a5d3ab
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-04-10 12:44:05 +0200

    docs: for SteamManager

--- FairyTaleDefender/Assets/_Game/Scripts/Common/Integrations/Steam/SteamManager.cs
+++ FairyTaleDefender/Assets/_Game/Scripts/Common/Integrations/Steam/SteamManager.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 namespace BoundfoxStudios.FairyTaleDefender.Common.Integrations.Steam
 {
+	/// <summary>
+	/// This file contains the <see cref="SteamManager"/> to define the API for the real implementation called "SteamManagerImpl".
+	/// If the Steam integration is turned of, this mock implementation is used.
+	/// We can use it to define mock data that is used during the development of the game.
+	/// When the game is being built, we'll enable the real Steam integration and get real data.
+	///
+	/// If you want to test the Steam integration locally, you can enable the Steam integration via the Unity menu:
+	/// "Fairy Tale Defender" -> Windows -> "Steam integration".
+	///
+	/// To actually use the SteamManager in the game, please use the SteamRuntimeAnchor that will hold an instance
+	/// of the correct class.
+	/// </summary>
 	[AddComponentMenu("")] // Hide in component menu.
 	public class SteamManager : MonoBehaviour
 	{
