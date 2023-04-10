4651e08229b5bec2121905ce6165e2ce2ffcf39c
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-04-10 11:57:14 +0200

    feat: improve steam integration

---
+++ FairyTaleDefender/Assets/_Game/Scripts/Common/Integrations/Steam/SteamManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace BoundfoxStudios.FairyTaleDefender.Common.Integrations.Steam
+{
+	public class SteamManager : MonoBehaviour
+	{
+		public virtual void Initialize() { }
+	}
+}
