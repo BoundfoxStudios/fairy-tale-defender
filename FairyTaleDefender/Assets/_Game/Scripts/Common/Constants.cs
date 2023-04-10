adda115a41df96b27fb933f33ebcaa6015b900f6
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-04-10 12:19:43 +0200

    refactor: rename the main menu name

--- FairyTaleDefender/Assets/_Game/Scripts/Common/Constants.cs
+++ FairyTaleDefender/Assets/_Game/Scripts/Common/Constants.cs
@@ -4,7 +4,7 @@ namespace BoundfoxStudios.FairyTaleDefender.Common
 	{
 		public static class MenuNames
 		{
-			public const string MenuName = "Community Project";
+			public const string MenuName = "Fairy Tale Defender";
 			public const string Events = MenuName + "/Events";
 			public const string SceneManagement = MenuName + "/Scene Management";
 			public const string UI = MenuName + "/UI";
