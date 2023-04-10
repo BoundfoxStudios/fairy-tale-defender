fc0f801490f786621dd6c861aa9427ad7df75a29
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-04-10 12:20:06 +0200

    feat: editor window for enabling the steam integration

--- FairyTaleDefender/Assets/_Game/Scripts/Common/Integrations/Steam/SteamManager.cs
+++ FairyTaleDefender/Assets/_Game/Scripts/Common/Integrations/Steam/SteamManager.cs
@@ -4,6 +4,9 @@ namespace BoundfoxStudios.FairyTaleDefender.Common.Integrations.Steam
 {
 	public class SteamManager : MonoBehaviour
 	{
-		public virtual void Initialize() { }
+		public virtual void Initialize()
+		{
+			Debug.Log("Steam integration has been turned off. To enable go to Fairy Tale Defender -> Windows -> Steam integration");
+		}
 	}
 }
