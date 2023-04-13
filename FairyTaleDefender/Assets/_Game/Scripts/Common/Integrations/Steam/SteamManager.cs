913222877b32e4938997ae2027fc3b4701aade02
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-04-13 20:22:29 +0200

    docs: typo

--- FairyTaleDefender/Assets/_Game/Scripts/Common/Integrations/Steam/SteamManager.cs
+++ FairyTaleDefender/Assets/_Game/Scripts/Common/Integrations/Steam/SteamManager.cs
@@ -6,7 +6,7 @@ namespace BoundfoxStudios.FairyTaleDefender.Common.Integrations.Steam
 	///   <para>
 	///     This file contains the <see cref="SteamManager" /> to define the API for the real implementation called
 	///     "SteamManagerImpl".
-	///     If the Steam integration is turned of, this mock implementation is used.
+	///     If the Steam integration is turned off, this mock implementation is used.
 	///     We can use it to define mock data that is used during the development of the game.
 	///     When the game is being built, we'll enable the real Steam integration and get real data.
 	///   </para>
