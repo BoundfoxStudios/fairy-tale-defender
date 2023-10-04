da4f4df475adbe8741bdaa977620597213a9bf61
M4lph <malph@live.de>
2023-10-04 12:33:43 +0200

    feat: Add event channel listeners

---
+++ FairyTaleDefender/Assets/_Game/Scripts/Runtime/Infrastructure/Events/Listener/BuildableEventChannelListener.cs
@@ -0,0 +1,12 @@
+﻿using BoundfoxStudios.FairyTaleDefender.Common;
+using BoundfoxStudios.FairyTaleDefender.Infrastructure.Events.ScriptableObjects;
+using UnityEngine;
+
+namespace BoundfoxStudios.FairyTaleDefender.Infrastructure.Events.Listener
+{
+	[AddComponentMenu(Constants.MenuNames.Events + "/Buildable Event Channel Listener")]
+	public class BuildableEventChannelListener : EventChannelListener<BuildableEventChannelSO, BuildableEventChannelSO.EventArgs>
+	{
+
+	}
+}
---
+++ FairyTaleDefender/Assets/_Game/Scripts/Runtime/Infrastructure/Events/Listener/VoidEventChannelListener.cs
@@ -0,0 +1,29 @@
+﻿using BoundfoxStudios.FairyTaleDefender.Common;
+using BoundfoxStudios.FairyTaleDefender.Infrastructure.Events.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BoundfoxStudios.FairyTaleDefender.Infrastructure.Events.Listener
+{
+	[AddComponentMenu(Constants.MenuNames.Events + "/Void Event Channel Listener")]
+	public class VoidEventChannelListener : MonoBehaviour
+	{
+		[field: SerializeField] public VoidEventChannelSO EventChannel { get; private set; } = default!;
+		[field: SerializeField] public UnityEvent EventResponse { get; private set; } = default!;
+
+		private void OnEnable()
+		{
+			EventChannel.Raised += Respond;
+		}
+
+		private void OnDisable()
+		{
+			EventChannel.Raised -= Respond;
+		}
+
+		private void Respond()
+		{
+			EventResponse.Invoke();
+		}
+	}
+}
