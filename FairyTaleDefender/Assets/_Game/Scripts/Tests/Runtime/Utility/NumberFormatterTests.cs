bf39533ccb6f0cfedda6947ed5587d5b2e375c95
M4lph <malph@live.de>
2024-01-08 10:26:39 +0100

    test(NumberFormatter): Add some tests

---
+++ FairyTaleDefender/Assets/_Game/Scripts/Tests/Runtime/Utility/NumberFormatterTests.cs
@@ -0,0 +1,21 @@
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine.Localization.Settings;
+
+namespace BoundfoxStudios.FairyTaleDefender.Tests.Utility
+{
+	public class NumberFormatterTests
+	{
+		[Test]
+		public void Format_DisplaysSeparatorInCurrentCulture()
+		{
+			var value = -1.5f;
+			var culture = LocalizationSettings.SelectedLocale.Identifier.CultureInfo;
+			var expectedResult = value.ToString("0.##", culture);
+
+			var result = FairyTaleDefender.Utility.NumberFormatter.Format(value);
+
+			result.Should().Be(expectedResult);
+		}
+	}
+}
