dbdb748ea0e4214f164028cfe6a3f705c4d56b21
M4lph <malph@live.de>
2024-02-04 10:01:18 +0100

    feat: level finished UI
    Display english/german localized UI as soon as a player finishes a level. Sets corresponding text to display won/lost status

--- FairyTaleDefender/Assets/_Game/Scripts/Runtime/Systems/GameplaySystem/GameplayController.cs
+++ FairyTaleDefender/Assets/_Game/Scripts/Runtime/Systems/GameplaySystem/GameplayController.cs
@@ -1,6 +1,5 @@
 using BoundfoxStudios.FairyTaleDefender.Common;
 using BoundfoxStudios.FairyTaleDefender.Infrastructure.Events.ScriptableObjects;
-using BoundfoxStudios.FairyTaleDefender.Infrastructure.SceneManagement.ScriptableObjects;
 using UnityEngine;
 
 namespace BoundfoxStudios.FairyTaleDefender.Systems.GameplaySystem
@@ -8,11 +7,7 @@ namespace BoundfoxStudios.FairyTaleDefender.Systems.GameplaySystem
 	[AddComponentMenu(Constants.MenuNames.GameplaySystem + "/" + nameof(GameplayController))]
 	public class GameplayController : MonoBehaviour
 	{
-		// TODO: Will be removed later
 		[field: Header("References")]
-		[field: SerializeField]
-		private MenuSO MainMenuScene { get; set; } = default!;
-
 		[field: Header("Listening Channels")]
 		[field: SerializeField]
 		private VoidEventChannelSO SceneReadyEventChannel { get; set; } = default!;
@@ -23,6 +18,9 @@ namespace BoundfoxStudios.FairyTaleDefender.Systems.GameplaySystem
 		[field: SerializeField]
 		private VoidEventChannelSO GameOverEventChannel { get; set; } = default!;
 
+		[field: SerializeField]
+		private VoidEventChannelSO AllObjectivesCompletedEventChannel { get; set; } = default!;
+
 		[field: Header("Broadcasting Channels")]
 		[field: SerializeField]
 		private VoidEventChannelSO GameplayStartEventChannel { get; set; } = default!;
@@ -31,13 +29,14 @@ namespace BoundfoxStudios.FairyTaleDefender.Systems.GameplaySystem
 		private VoidEventChannelSO SpawnNextWaveEventChannel { get; set; } = default!;
 
 		[field: SerializeField]
-		private LoadSceneEventChannelSO LoadSceneEventChannel { get; set; } = default!;
+		private BoolEventChannelSO LevelFinishedEventChannel { get; set; } = default!;
 
 		private void OnEnable()
 		{
 			SceneReadyEventChannel.Raised += SceneReady;
 			WaveSpawnedEventChannel.Raised += WaveSpawned;
 			GameOverEventChannel.Raised += GameOver;
+			AllObjectivesCompletedEventChannel.Raised += ObjectivesCompleted;
 		}
 
 		private void OnDisable()
@@ -45,12 +44,22 @@ namespace BoundfoxStudios.FairyTaleDefender.Systems.GameplaySystem
 			SceneReadyEventChannel.Raised -= SceneReady;
 			WaveSpawnedEventChannel.Raised -= WaveSpawned;
 			GameOverEventChannel.Raised -= GameOver;
+			AllObjectivesCompletedEventChannel.Raised -= ObjectivesCompleted;
+		}
+
+		private void ObjectivesCompleted()
+		{
+			FinishLevel(true);
 		}
 
 		private void GameOver()
 		{
-			// TODO: Will be removed later
-			LoadSceneEventChannel.Raise(new() { Scene = MainMenuScene });
+			FinishLevel(false);
+		}
+
+		private void FinishLevel(bool playerWon)
+		{
+			LevelFinishedEventChannel.Raise(playerWon);
 		}
 
 		private void WaveSpawned(bool hasMoreWaves)
