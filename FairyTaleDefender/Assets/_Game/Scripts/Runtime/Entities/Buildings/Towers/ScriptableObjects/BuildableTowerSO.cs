40f4ea2873aea991c13e234708c8e0bfc46781f6
M4lph <malph@live.de>
2024-02-08 17:14:09 +0100

    feat: create buttons to build towers dynamically

--- FairyTaleDefender/Assets/_Game/Scripts/Runtime/Entities/Buildings/Towers/ScriptableObjects/BuildableTowerSO.cs
+++ FairyTaleDefender/Assets/_Game/Scripts/Runtime/Entities/Buildings/Towers/ScriptableObjects/BuildableTowerSO.cs
@@ -1,19 +1,27 @@
 using BoundfoxStudios.FairyTaleDefender.Common;
+using BoundfoxStudios.FairyTaleDefender.Entities.Weapons.ScriptableObjects;
 using BoundfoxStudios.FairyTaleDefender.Systems.BuildSystem;
 using UnityEngine;
-using UnityEngine.Localization;
 
 namespace BoundfoxStudios.FairyTaleDefender.Entities.Buildings.Towers.ScriptableObjects
 {
 	[CreateAssetMenu(menuName = Constants.MenuNames.Towers + "/Buildable Tower")]
 	public class BuildableTowerSO : TowerSO, IAmBuildable, IHaveAPrice
 	{
+		[field: Header("References")]
 		[field: SerializeField]
 		public GameObject Prefab { get; private set; } = default!;
 
 		[field: SerializeField]
 		public GameObject BlueprintPrefab { get; private set; } = default!;
 
+		[field: SerializeField]
+		public Sprite BuildableIcon { get; private set; } = default!;
+
+		[field: SerializeField]
+		public WeaponSO WeaponSO { get; private set; } = default!;
+
+		[field: Header("Settings")]
 		[field: SerializeField]
 		[field: Range(1, 1000)]
 		public int Price { get; private set; } = 100;
