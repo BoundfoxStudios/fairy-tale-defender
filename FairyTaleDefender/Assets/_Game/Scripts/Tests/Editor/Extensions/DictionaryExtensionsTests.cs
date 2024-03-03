1768ddf479c588085cf9da125fd0d1bbd8d0e6cf
M4lph <malph@live.de>
2024-03-03 10:56:06 +0100

    test: add tests for float and dictionary extensionsmethods

---
+++ FairyTaleDefender/Assets/_Game/Scripts/Tests/Editor/Extensions/DictionaryExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BoundfoxStudios.FairyTaleDefender.Extensions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BoundfoxStudios.FairyTaleDefender.Tests.Editor.Extensions
+{
+	public class DictionaryExtensionsTests
+	{
+		[Test]
+		public void TryAddLazy_DoesNotAddItemToDictionary_IfKeyAlreadyExists()
+		{
+			var sut = new Dictionary<int, string> { {1, "1"} };
+			var newEntry = 1;
+
+			sut.TryAddLazy(newEntry, () => newEntry.ToString());
+
+			sut.Count.Should().Be(1);
+		}
+
+		[Test]
+		public void TryAddLazy_DoesAddItemToDictionary_IfKeyIsNew()
+		{
+			var sut = new Dictionary<int, string> { {1, "1"} };
+			var newEntry = 2;
+			var kvp = new KeyValuePair<int, string>(newEntry, newEntry.ToString());
+
+			sut.TryAddLazy(newEntry, () => newEntry.ToString());
+
+			sut.Should().Contain(kvp);
+		}
+
+		[Test]
+		public void TryAddLazy_DoesThrow_IfDictionaryIsNull()
+		{
+			var sut = new Dictionary<int, string>();
+			sut = null;
+
+			var newEntry = 2;
+			TestDelegate action = () => sut.TryAddLazy(newEntry, () => newEntry.ToString());
+
+			Assert.Throws<NullReferenceException>(action);
+		}
+	}
+}
