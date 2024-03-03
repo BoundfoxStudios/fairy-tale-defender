830ed935cdbf4ac22d1359a57fdb195ac2023624
M4lph <malph@live.de>
2024-03-03 08:53:43 +0100

    fix: implement suggested changes
    Co-authored-by: Manuel Rauber <manuel.rauber@boundfoxstudios.com>

--- FairyTaleDefender/Assets/_Game/Scripts/Runtime/Systems/InputSystem/ScriptableObjects/CallbackProcessors/PauseActionsSO.cs
+++ FairyTaleDefender/Assets/_Game/Scripts/Runtime/Systems/InputSystem/ScriptableObjects/CallbackProcessors/PauseActionsSO.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 namespace BoundfoxStudios.FairyTaleDefender.Systems.InputSystem.ScriptableObjects.CallbackProcessors
 {
 	//We don't need more than one instance of this.
-	[CreateAssetMenu(menuName = Constants.MenuNames.Input + "/Pause Actions")]
+	// [CreateAssetMenu(menuName = Constants.MenuNames.Input + "/Pause Actions")]
 	public class PauseActionsSO : ScriptableObject, GameInput.IPauseActions
 	{
 		public event Action ResumeGame = delegate { };
