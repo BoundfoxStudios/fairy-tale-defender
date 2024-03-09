09e4015534505d1bcbed500bfe41e6414ade6df2
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2024-03-09 08:09:23 +0100

    refactor: use DirectoryManager in the FileManagerMenus

--- FairyTaleDefender/Assets/_Game/Scripts/Editor/Menus/FileManagerMenus.cs
+++ FairyTaleDefender/Assets/_Game/Scripts/Editor/Menus/FileManagerMenus.cs
@@ -1,4 +1,6 @@
 using System.IO;
+using BoundfoxStudios.FairyTaleDefender.Infrastructure.FileManagement;
+using Cysharp.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 
@@ -15,40 +17,52 @@ namespace BoundfoxStudios.FairyTaleDefender.Editor.Menus
 		[MenuItem(Constants.MenuNames.Files + "/Delete Default Save Game", priority = 1000)]
 		private static void DeleteDefaultSaveGame()
 		{
-			var path = Path.Combine(Application.persistentDataPath, Common.Constants.SaveGames.DirectoryName,
-				Common.Constants.SaveGames.DefaultSaveGameName);
+			DeleteDefaultSaveGameAsync().Forget();
+		}
 
-			if (!Directory.Exists(path))
+		private static async UniTaskVoid DeleteDefaultSaveGameAsync()
+		{
+			var directoryManager = new DirectoryManager();
+			var path = Path.Combine(Common.Constants.SaveGames.DirectoryName, Common.Constants.SaveGames.DefaultSaveGameName);
+
+			if (!await directoryManager.ExistsAsync(path))
 			{
 				Debug.Log($"Path \"{path}\" does not exist. Did you already save a game?");
 				return;
 			}
 
-			Directory.Delete(path, true);
+			await directoryManager.DeleteAsync(path);
 			Debug.Log("Default Save Game has been deleted!");
 		}
+
 
 		[MenuItem(Constants.MenuNames.Files + "/Delete All Save Games", priority = 1001)]
 		private static void DeleteAllSaveGames()
 		{
-			var path = Path.Combine(Application.persistentDataPath, Common.Constants.SaveGames.DirectoryName);
+			DeleteAllSaveGamesAsync().Forget();
+		}
 
-			if (!Directory.Exists(path))
+		private static async UniTaskVoid DeleteAllSaveGamesAsync()
+		{
+			var directoryManager = new DirectoryManager();
+			var path = Common.Constants.SaveGames.DirectoryName;
+
+			if (!await directoryManager.ExistsAsync(path))
 			{
 				Debug.Log($"Path \"{path}\" does not exist. Did you already save a game?");
 				return;
 			}
 
 			var confirmDeletion = EditorUtility.DisplayDialog("Confirm deletion",
-				"Do you really want to delete all save games?", "Yes, delete all",
-				"No!");
+				"Do you really want to delete all save games?",
+				"Yes, delete all", "No!");
 
 			if (!confirmDeletion)
 			{
 				return;
 			}
 
-			Directory.Delete(path, true);
+			await directoryManager.DeleteAsync(path);
 			Debug.Log("All Save Games have been deleted!");
 		}
 	}
